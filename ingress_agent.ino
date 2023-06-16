dd4e312f8e1c974ed267f952dfc867b8cf579735
Bart Huylebroeck <bahoeman+git@gmail.com>
2023-06-16 20:30:51 +0200

    first attempt switching colors. Needs proper disconnect.

--- ingress_agent.ino
+++ ingress_agent.ino
@@ -2,11 +2,13 @@
 //#include <BLEScan.h>
 #include <Fri3dMatrix.h>
 #include <Fri3dButtons.h>
+#include <Fri3dBuzzer.h>
 
 #include "ingress.h"
 
 Fri3dMatrix matrix = Fri3dMatrix();
 Fri3dButtons buttons = Fri3dButtons();
+Fri3dBuzzer buzzer = Fri3dBuzzer();
 
 // UUIDs were generated randomly using https://www.uuidgenerator.net/
 static BLEUUID FRI3D_SERVICE_UUID("ea5c0c62-079e-11ee-be56-0242ac120002");
@@ -14,6 +16,7 @@ static BLEUUID   FRI3D_COLOR_UUID("086aece6-079f-11ee-be56-0242ac120002");
 static BLEUUID    FRI3D_PUSH_UUID("3ed75c2e-079f-11ee-be56-0242ac120002");
 
 BLEAdvertisedDevice *closestBeacon;
+BLERemoteService *closestService;
 int closestRSSI = -1000;
 uint8_t myColor = CAMP_BLUE;
 
@@ -70,6 +73,7 @@ void setup() {
   Serial.println("Code for Ingress agent on Fri3d Badge 2018 (wolf)");
   
   init_bluetooth();
+  showColor();
 }
 
 bool ConnectToBeacon()
@@ -77,18 +81,19 @@ bool ConnectToBeacon()
   Serial.println("attempting to connect...");
   BLEClient*  pClient  = BLEDevice::createClient();
   pClient->connect(closestBeacon);
-  BLERemoteService* pRemoteService = pClient->getService(FRI3D_SERVICE_UUID);
-  if (pRemoteService == nullptr) {
+  closestService = pClient->getService(FRI3D_SERVICE_UUID);
+  if (closestService == nullptr) {
     Serial.print("Failed to find our service FRI3D_SERVICE_UUID");
     pClient->disconnect();
+    closestService = nullptr;
     return false;
   }
   else {
     Serial.print("service found: ");
-    Serial.println(pRemoteService->toString().c_str());
+    Serial.println(closestService->toString().c_str());
   }
 
-  BLERemoteCharacteristic *colorChar = pRemoteService->getCharacteristic(FRI3D_COLOR_UUID);
+  BLERemoteCharacteristic *colorChar = closestService->getCharacteristic(FRI3D_COLOR_UUID);
   if (colorChar == nullptr) {
     Serial.println("Failed to find FRI3D_COLOR_UUID");
   }
@@ -97,23 +102,12 @@ bool ConnectToBeacon()
     Serial.println(colorChar->readValue().c_str());
   }
 
-  BLERemoteCharacteristic *pushChar = pRemoteService->getCharacteristic(FRI3D_PUSH_UUID);
-  if (pushChar == nullptr) {
-    Serial.println("Failed to find FRI3D_PUSH_UUID");
-  }
-  else {
-    Serial.print("can read? "); Serial.println(pushChar->canRead());
-    Serial.print("can write? "); Serial.println(pushChar->canWrite());
-    Serial.print("can write no response? "); Serial.println(pushChar->canWriteNoResponse());
-    uint8_t mijnkamp = CAMP_BLUE;
-    pushChar->writeValue(&mijnkamp, 1);
-  }
 
-  pClient->disconnect();
-  return false;
+  return true;
 }
 
-void scanAndIntrude() {
+bool findClosestBeacon()
+{
   //first, reset distance so we can try and find closest beacon:
   closestRSSI = -1000;
 
@@ -123,22 +117,88 @@ void scanAndIntrude() {
   pBLEScan->setAdvertisedDeviceCallbacks(new AgentAdvertisedDeviceCallbacks());
   pBLEScan->start(5, false);
 
-  if (closestRSSI > -1000) ConnectToBeacon();
-  Serial.println(scanDone.c_str());
-  int x = -14; //there are 14 columns of LEDs, start at outside
-  while(!(buttons.getButton(0) || buttons.getButton(1))) {
-    matrix.clear();
-    matrix.drawString( -x, scanDone );
-    x++;
-    if (x>((int)scanDone.length() * 4)) x=-14;
-    delay(100);
-  }
+  if (closestRSSI > -1000) return true;
+  return false;
 }
 
-void switchColor() {
+void showColor()
+{
+  matrix.clear();
+  matrix.drawString(0, "1=");
+  if (myColor == CAMP_RED) matrix.drawCharacter(7, 'R');
+  else if (myColor == CAMP_BLUE) matrix.drawCharacter(7, 'B');
+  else matrix.drawCharacter(7, 'w');
+}
 
+void switchColor()
+{
+  if (myColor == CAMP_RED) myColor = CAMP_BLUE;
+  else if (myColor == CAMP_BLUE) myColor = CAMP_RED;
+  else myColor = CAMP_BLUE;
+}
+
+bool Invade()
+{
+  if (closestBeacon == nullptr) {
+    Serial.println("Invasion impossible; no beacon available");
+    return false;
+  }
+  if (closestService == nullptr) {
+    Serial.println("Invasion impossible; no service for beacon");
+    return false;
+  }
+
+  BLERemoteCharacteristic *pushChar = closestService->getCharacteristic(FRI3D_PUSH_UUID);
+  if (pushChar == nullptr) {
+    Serial.println("Failed to find FRI3D_PUSH_UUID");
+    return false;
+  }
+  else {
+    Serial.print("can read? "); Serial.println(pushChar->canRead());
+    Serial.print("can write? "); Serial.println(pushChar->canWrite());
+    Serial.print("can write no response? "); Serial.println(pushChar->canWriteNoResponse());
+    pushChar->writeValue(&myColor, 1);
+  }
+  return true;
 }
 
 void loop() {
-  scanAndIntrude();
+  if (buttons.getButton(0)) {
+    switchColor();
+    matrix.clear();
+    delay(200);
+    showColor();
+  }
+
+  if (buttons.getButton(1)) {
+    matrix.clear();
+    if (
+         findClosestBeacon()
+      && ConnectToBeacon()
+      && Invade()
+     )
+    {
+      Serial.println("Successfully invaded beacon");
+      buzzer.setFrequency(1000);
+      buzzer.setVolume(200);
+      delay(50);
+      buzzer.setFrequency(10000);
+      buzzer.setVolume(200);
+      delay(50);
+      buzzer.setVolume(0);
+      showColor();
+      delay(200);
+    }
+    else {
+      buzzer.setFrequency(10000);
+      buzzer.setVolume(200);
+      delay(50);
+      buzzer.setFrequency(100);
+      buzzer.setVolume(200);
+      delay(50);
+      buzzer.setVolume(0);
+      showColor();
+      delay(200);
+    }
+  }
 }
